dabe92a3c63b5b936024166923ace76a9e5d78f5
gahyeon <1000ji1@naver.com>
2023-11-05 00:51:13 +0900

    feat: setup project

---
+++ src/main/java/dev/line4/blackBoard/BlackBoardApplication.java
@@ -0,0 +1,13 @@
+package dev.line4.blackBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackBoardApplication.class, args);
+	}
+
+}
