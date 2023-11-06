b2fe60b2028b9712968a244b3a95253006627b6b
MinseoKangQ <kms02171@naver.com>
2023-11-06 13:48:56 +0900

    feat: Add stereotype associated with letter entity

---
+++ src/main/java/dev/line4/blackBoard/letter/dto/LetterReqDto.java
@@ -0,0 +1,11 @@
+package dev.line4.blackBoard.letter.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class LetterReqDto {
+}
---
+++ src/main/java/dev/line4/blackBoard/letter/service/LetterService.java
@@ -0,0 +1,4 @@
+package dev.line4.blackBoard.letter.service;
+
+public interface LetterService {
+}
