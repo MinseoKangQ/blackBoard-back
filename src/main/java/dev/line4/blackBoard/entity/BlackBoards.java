4f10a5059c3d98868c53b413843b2ddcb24f725c
gahyeon <1000ji1@naver.com>
2023-11-07 15:58:24 +0900

    feat: Add OneToMany relationship

--- src/main/java/dev/line4/blackBoard/entity/BlackBoards.java
+++ src/main/java/dev/line4/blackBoard/entity/BlackBoards.java
@@ -1,11 +1,14 @@
 package dev.line4.blackBoard.entity;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -35,4 +38,6 @@ public class BlackBoards {
     private LocalDateTime graduateDate;
     @Column(columnDefinition = "TEXT")
     private String url;
+    @OneToMany(mappedBy = "boardId", fetch = FetchType.LAZY)
+    private Set<BlackBoardStickers> blackBoardStickers;
 }
