c9c7b830b7dd5293fb9565eda1c60d15f449130b
MinseoKangQ <kms02171@naver.com>
2023-11-07 15:14:45 +0900

    feat: Add blackboard FK to letter entity, Add stereotype associated with readVisitor method

--- src/main/java/dev/line4/blackBoard/entity/BlackBoardsEntity.java
+++ src/main/java/dev/line4/blackBoard/entity/BlackBoardsEntity.java
@@ -1,11 +1,17 @@
 package dev.line4.blackBoard.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.line4.blackBoard.letter.entity.Letters;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /*
 id long
@@ -32,6 +38,10 @@ public class BlackBoardsEntity {
     private LocalDateTime graduate_date;
     @Column(columnDefinition = "TEXT")
     private String url;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "blackboard", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Letters> letters = new ArrayList<>();
 
     public Long getId() {
         return id;
---
+++ src/main/java/dev/line4/blackBoard/letter/dto/VisitorResDto.java
@@ -0,0 +1,17 @@
+package dev.line4.blackBoard.letter.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VisitorResDto {
+    private List<String> nickname;
+}
