43b051bb127db8cf595e9f1899a8d3cd6f80d480
gahyeon <1000ji1@naver.com>
2023-11-09 20:07:06 +0900

    fix: Save BlackBoardStickers

--- src/main/java/dev/line4/blackBoard/blackboard/service/BlackBoardService.java
+++ src/main/java/dev/line4/blackBoard/blackboard/service/BlackBoardService.java
@@ -4,15 +4,10 @@ import dev.line4.blackBoard.blackboard.dto.BlackBoardReqDto;
 import dev.line4.blackBoard.blackboard.dto.BlackBoardResDto;
 import dev.line4.blackBoard.blackboard.entity.BlackBoards;
 import dev.line4.blackBoard.blackboard.repository.BlackBoardRepository;
-import dev.line4.blackBoard.blackboardsticker.dto.BlackBoardStickerDto;
-import dev.line4.blackBoard.blackboardsticker.entity.BlackBoardStickers;
 import dev.line4.blackBoard.blackboardsticker.service.BlackBoardStickerService;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,27 +35,18 @@ public class BlackBoardService {
                 .introduction(blackBoardReqDto.getIntroduction())
                 .email(blackBoardReqDto.getEmail())
                 .graduateDate(parseGraduateDate(blackBoardReqDto.getGraduateDate()))
-                .blackBoardStickers(
-                        mapToBlackBoardStickers(blackBoardReqDto.getStickers())) // BlackBoardSticker record도 생성
                 .build();
-        blackBoardRepository.save(blackBoards);
+        BlackBoards savedBlackBoard = blackBoardRepository.save(blackBoards);
 
+        blackBoardStickerService.createBlackBoardStickers(blackBoardReqDto.getStickers(), savedBlackBoard);
+        
         return BlackBoardResDto.builder().url(randomUrl).build();
     }
-    
+
     private LocalDateTime parseGraduateDate(String graduateDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return LocalDateTime.parse(graduateDate, formatter);
     }
 
-    private Set<BlackBoardStickers> mapToBlackBoardStickers(List<BlackBoardStickerDto> stickerDtos) {
-        return stickerDtos.stream()
-                .map(this::mapToBlackBoardSticker)
-                .collect(Collectors.toSet());
-    }
-
-    private BlackBoardStickers mapToBlackBoardSticker(BlackBoardStickerDto stickerDto) {
-        return blackBoardStickerService.createBlackBoardStickers(stickerDto);
-    }
 
 }
--- src/main/java/dev/line4/blackBoard/blackboardsticker/service/BlackBoardStickerService.java
+++ src/main/java/dev/line4/blackBoard/blackboardsticker/service/BlackBoardStickerService.java
@@ -1,21 +1,36 @@
 package dev.line4.blackBoard.blackboardsticker.service;
 
+import dev.line4.blackBoard.blackboard.entity.BlackBoards;
 import dev.line4.blackBoard.blackboardsticker.dto.BlackBoardStickerDto;
 import dev.line4.blackBoard.blackboardsticker.entity.BlackBoardStickers;
+import dev.line4.blackBoard.blackboardsticker.repository.BlackBoardStickerRepository;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BlackBoardStickerService {
+    private final BlackBoardStickerRepository blackBoardStickerRepository;
 
-    public BlackBoardStickers createBlackBoardStickers(BlackBoardStickerDto stickerDto) {
-        return BlackBoardStickers.builder()
-                .num(stickerDto.getNum())
-                .positionX(stickerDto.getPositionX())
-                .positionY(stickerDto.getPositionY())
-                .img(stickerDto.getImg())
-                .width(stickerDto.getWidth())
-                .angle(stickerDto.getAngle())
-                .mirror(stickerDto.getMirror())
-                .build();
+    public BlackBoardStickerService(BlackBoardStickerRepository blackBoardStickerRepository) {
+        this.blackBoardStickerRepository = blackBoardStickerRepository;
+    }
+
+    public void createBlackBoardStickers(List<BlackBoardStickerDto> stickerDtos, BlackBoards blackboard) {
+
+        for (BlackBoardStickerDto stickerDto : stickerDtos) {
+            BlackBoardStickers blackBoardStickers = BlackBoardStickers.builder()
+                    .num(stickerDto.getNum())
+                    .positionX(stickerDto.getPositionX())
+                    .positionY(stickerDto.getPositionY())
+                    .img(stickerDto.getImg())
+                    .width(stickerDto.getWidth())
+                    .angle(stickerDto.getAngle())
+                    .mirror(stickerDto.getMirror())
+                    .boardId(blackboard)
+                    .build();
+
+            blackBoardStickerRepository.save(blackBoardStickers);
+        }
+
     }
 }
