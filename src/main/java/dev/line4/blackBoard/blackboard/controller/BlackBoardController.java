4942ec03eec48978680e30aa642eda01028d920c
gahyeon <1000ji1@naver.com>
2023-11-10 01:25:23 +0900

    feat: Implement Blackboard disclosure api

--- src/main/java/dev/line4/blackBoard/blackboard/controller/BlackBoardController.java
+++ src/main/java/dev/line4/blackBoard/blackboard/controller/BlackBoardController.java
@@ -1,9 +1,11 @@
 package dev.line4.blackBoard.blackboard.controller;
 
 import dev.line4.blackBoard.blackboard.dto.BlackBoardCountDto;
+import dev.line4.blackBoard.blackboard.dto.BlackBoardOpenResDto;
 import dev.line4.blackBoard.blackboard.dto.BlackBoardReqDto;
 import dev.line4.blackBoard.blackboard.dto.BlackBoardResDto;
 import dev.line4.blackBoard.blackboard.service.BlackBoardService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,13 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlackBoardController {
     @Autowired
     private final BlackBoardService blackBoardService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public BlackBoardController(BlackBoardService blackBoardService) {
+    public BlackBoardController(BlackBoardService blackBoardService, ModelMapper modelMapper) {
         this.blackBoardService = blackBoardService;
+        this.modelMapper = modelMapper;
     }
 
-    @GetMapping("/blackboard")
+    @GetMapping("/blackboards")
     public BlackBoardCountDto getTotalRecordCount() {
         return BlackBoardCountDto.builder()
                 .blackboard_count(blackBoardService.getTotalRecordCount())
@@ -43,5 +48,13 @@ public class BlackBoardController {
         // 생성 성공 응답
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/blackboard")
+    public ResponseEntity<BlackBoardOpenResDto> getBlackBoardAndLetter(@RequestParam("id") String blackboardId) {
+        BlackBoardOpenResDto blackBoardOpenResDto = blackBoardService.getBlackBoardAndLetter(blackboardId);
+
+        BlackBoardOpenResDto responseDto = modelMapper.map(blackBoardOpenResDto, BlackBoardOpenResDto.class);
+        return ResponseEntity.ok(responseDto);
     }
 }
--- src/main/java/dev/line4/blackBoard/blackboard/service/BlackBoardService.java
+++ src/main/java/dev/line4/blackBoard/blackboard/service/BlackBoardService.java
@@ -1,13 +1,23 @@
 package dev.line4.blackBoard.blackboard.service;
 
+import dev.line4.blackBoard.blackboard.dto.BlackBoardOpenResDto;
 import dev.line4.blackBoard.blackboard.dto.BlackBoardReqDto;
 import dev.line4.blackBoard.blackboard.dto.BlackBoardResDto;
 import dev.line4.blackBoard.blackboard.entity.BlackBoards;
 import dev.line4.blackBoard.blackboard.repository.BlackBoardRepository;
+import dev.line4.blackBoard.blackboardsticker.dto.BlackBoardStickerResDto;
+import dev.line4.blackBoard.blackboardsticker.entity.BlackBoardStickers;
 import dev.line4.blackBoard.blackboardsticker.service.BlackBoardStickerService;
+import dev.line4.blackBoard.letter.dto.LetterOpenResDto;
+import dev.line4.blackBoard.letter.entity.Letters;
+import dev.line4.blackBoard.lettersticker.dto.LetterStickerReqDto;
+import dev.line4.blackBoard.lettersticker.entity.LetterStickers;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -39,7 +49,7 @@ public class BlackBoardService {
         BlackBoards savedBlackBoard = blackBoardRepository.save(blackBoards);
 
         blackBoardStickerService.createBlackBoardStickers(blackBoardReqDto.getStickers(), savedBlackBoard);
-        
+
         return BlackBoardResDto.builder().url(randomUrl).build();
     }
 
@@ -48,5 +58,82 @@ public class BlackBoardService {
         return LocalDateTime.parse(graduateDate, formatter);
     }
 
+    public BlackBoardOpenResDto getBlackBoardAndLetter(String blackboardId) {
+        BlackBoards blackBoard = blackBoardRepository.findById(blackboardId)
+                .orElseThrow(() -> new RuntimeException("Blackboard not found with id: " + blackboardId));
+        List<BlackBoardStickerResDto> boardStickers = mapToBlackBoardStickerResDtos(blackBoard.getBlackBoardStickers());
+        List<LetterOpenResDto> letters = mapToLetterOpenResDtos(blackBoard.getLetters());
 
+        return BlackBoardOpenResDto.builder()
+                .title(blackBoard.getTitle())
+                .introduction(blackBoard.getIntroduction())
+                .graduateDate(formatGraduateDate(blackBoard.getGraduateDate()))
+                .boardStickers(boardStickers)
+                .letters(letters)
+                .build();
+    }
+
+    // localtime -> string
+    private String formatGraduateDate(LocalDateTime graduateDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return graduateDate.format(formatter);
+    }
+
+    private List<BlackBoardStickerResDto> mapToBlackBoardStickerResDtos(Set<BlackBoardStickers> blackBoardStickers) {
+        return blackBoardStickers.stream()
+                .map(this::mapToBlackBoardStickerResDto)
+                .collect(Collectors.toList());
+    }
+
+    // blackboard에 sticker 연결
+    private BlackBoardStickerResDto mapToBlackBoardStickerResDto(BlackBoardStickers sticker) {
+        return BlackBoardStickerResDto.builder()
+                .num(sticker.getNum())
+                .positionX(sticker.getPositionX())
+                .positionY(sticker.getPositionY())
+                .img(sticker.getImg())
+                .width(sticker.getWidth())
+                .angle(sticker.getAngle())
+                .mirror(sticker.getMirror())
+                .build();
+    }
+
+    // blackboard에 letter 연결
+    private List<LetterOpenResDto> mapToLetterOpenResDtos(List<Letters> letters) {
+        return letters.stream()
+                .map(this::mapToLetterOpenResDto)
+                .collect(Collectors.toList());
+    }
+
+    private LetterOpenResDto mapToLetterOpenResDto(Letters letter) {
+        List<LetterStickerReqDto> stickers = mapToLetterStickerReqDtos(letter.getStickers());
+
+        return LetterOpenResDto.builder()
+                .id(letter.getLetterId())
+                .nickname(letter.getNickname())
+                .content(letter.getContent())
+                .font(letter.getFont())
+                .align(letter.getAlign())
+                .stickers(stickers)
+                .build();
+    }
+
+    // letter에 letterStickers 연결
+    private List<LetterStickerReqDto> mapToLetterStickerReqDtos(List<LetterStickers> letterStickers) {
+        return letterStickers.stream()
+                .map(this::mapToLetterStickerReqDto)
+                .collect(Collectors.toList());
+    }
+
+    private LetterStickerReqDto mapToLetterStickerReqDto(LetterStickers letterSticker) {
+        return LetterStickerReqDto.builder()
+                .num(letterSticker.getNum())
+                .positionX(letterSticker.getPositionX())
+                .positionY(letterSticker.getPositionY())
+                .img(letterSticker.getImg())
+                .width(letterSticker.getWidth())
+                .angle(letterSticker.getAngle())
+                .mirror(letterSticker.getMirror())
+                .build();
+    }
 }
