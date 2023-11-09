dbbeddf8faf30efef676b137e638a9530472178e
MinseoKangQ <kms02171@naver.com>
2023-11-09 17:17:54 +0900

    feat: Deploy this project

--- src/main/java/dev/line4/blackBoard/utils/cors/CorsConfig.java
+++ src/main/java/dev/line4/blackBoard/utils/cors/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://congchugraduate.netlify.app:443")
+                .allowedOrigins("https://congchugraduate.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
---
+++ src/main/java/dev/line4/blackBoard/utils/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package dev.line4.blackBoard.utils.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+// HTTP to HTTPS config
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity https) throws Exception {
+        return https
+                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll())
+                .build();
+    }
+
+}
