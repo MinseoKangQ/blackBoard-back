8b3f7db6bea7f35611cda50b46065eb4e3fefe60
gahyeon <1000ji1@naver.com>
2023-11-09 18:21:43 +0900

    Feat: Create BlackBoard record

--- src/main/java/dev/line4/blackBoard/blackboard/controller/BlackBoardController.java
+++ src/main/java/dev/line4/blackBoard/blackboard/controller/BlackBoardController.java
@@ -1,16 +1,23 @@
 package dev.line4.blackBoard.blackboard.controller;
 
+import dev.line4.blackBoard.blackboard.dto.BlackBoardReqDto;
+import dev.line4.blackBoard.blackboard.dto.BlackBoardResDto;
 import dev.line4.blackBoard.blackboard.service.BlackBoardService;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class BlackBoardController {
+    @Autowired
     private final BlackBoardService blackBoardService;
 
     @Autowired
@@ -18,10 +25,24 @@ public class BlackBoardController {
         this.blackBoardService = blackBoardService;
     }
 
-    @GetMapping("blackboard")
+    @GetMapping("/blackboard")
     public Map<String, Integer> getTotalRecordCount() {
         Map<String, Integer> response = new HashMap<>();
         response.put("blackboard_count", (int) blackBoardService.getTotalRecordCount());
         return response;
+    }
+
+    @PostMapping("/blackboard")
+    public ResponseEntity<BlackBoardResDto> createBlackBoard(@RequestBody BlackBoardReqDto blackBoardReqDto) {
+        BlackBoardResDto responseDto = blackBoardService.createBlackBoard(blackBoardReqDto);
+
+        if (responseDto.getUrl() == null) {
+            // 이미 생성한 경우, 실패 응답
+            return new ResponseEntity<>(responseDto, HttpStatus.CONFLICT);
+        }
+
+        // 생성 성공 응답
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+
     }
 }
--- src/main/java/dev/line4/blackBoard/blackboard/service/BlackBoardService.java
+++ src/main/java/dev/line4/blackBoard/blackboard/service/BlackBoardService.java
@@ -1,12 +1,26 @@
 package dev.line4.blackBoard.blackboard.service;
 
+import dev.line4.blackBoard.blackboard.dto.BlackBoardReqDto;
+import dev.line4.blackBoard.blackboard.dto.BlackBoardResDto;
+import dev.line4.blackBoard.blackboard.entity.BlackBoards;
 import dev.line4.blackBoard.blackboard.repository.BlackBoardRepository;
+import dev.line4.blackBoard.blackboardsticker.dto.BlackBoardStickerDto;
+import dev.line4.blackBoard.blackboardsticker.entity.BlackBoardStickers;
+import dev.line4.blackBoard.blackboardsticker.service.BlackBoardStickerService;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BlackBoardService {
     private final BlackBoardRepository blackBoardRepository;
+    @Autowired
+    private BlackBoardStickerService blackBoardStickerService;
 
     @Autowired
     public BlackBoardService(BlackBoardRepository blackBoardRepository) {
@@ -15,6 +29,38 @@ public class BlackBoardService {
 
     public long getTotalRecordCount() {
         return blackBoardRepository.count();
+    }
+
+    public BlackBoardResDto createBlackBoard(BlackBoardReqDto blackBoardReqDto) {
+
+        BlackBoards blackBoards = BlackBoards.builder()
+                .title(blackBoardReqDto.getTitle())
+                .introduction(blackBoardReqDto.getIntroduction())
+                .email(blackBoardReqDto.getEmail())
+                .graduateDate(parseGraduateDate(blackBoardReqDto.getGraduateDate()))
+                .blackBoardStickers(
+                        mapToBlackBoardStickers(blackBoardReqDto.getStickers())) // BlackBoardSticker record도 생성
+                .build();
+        blackBoardRepository.save(blackBoards);
+
+        String randomUrl = UUID.randomUUID().toString().substring(0, 12);
+        return BlackBoardResDto.builder().url(randomUrl).build();
+    }
+
+    // 졸업날짜 yyyy-mm-dd 형식
+    private LocalDateTime parseGraduateDate(String graduateDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return LocalDateTime.parse(graduateDate, formatter);
+    }
+
+    private Set<BlackBoardStickers> mapToBlackBoardStickers(List<BlackBoardStickerDto> stickerDtos) {
+        return stickerDtos.stream()
+                .map(this::mapToBlackBoardSticker)
+                .collect(Collectors.toSet());
+    }
+
+    private BlackBoardStickers mapToBlackBoardSticker(BlackBoardStickerDto stickerDto) {
+        return blackBoardStickerService.createBlackBoardStickers(stickerDto);
     }
 
 }
