4aad69fc45e85d13924571b15aebba817403d68d
MinseoKangQ <kms02171@naver.com>
2023-11-10 14:42:22 +0900

    fix: Allow post method

--- src/main/java/dev/line4/blackBoard/utils/cors/CorsConfig.java
+++ src/main/java/dev/line4/blackBoard/utils/cors/CorsConfig.java
@@ -1,11 +1,8 @@
 package dev.line4.blackBoard.utils.cors;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,13 +16,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("https://congchugraduate.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().anyRequest().permitAll();
-        return http.build();
     }
 
 }
